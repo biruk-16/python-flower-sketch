930cfaf5e750c29fddee18ac3185c4f731c449f7
biruk-21mack <burakiomass@gmail.com>
2023-03-23 16:05:07 +0300

    first commit

---
+++ flower design.py
@@ -0,0 +1,11 @@
+from turtle import *
+speed(20)
+def fleur():
+    for i in range(300):
+        circle(190-i , 90)
+        left(90)
+        circle(190-i , 90)
+        left(18)
+fleur()
+mainloop()
+
